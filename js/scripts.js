65508c6553dbb181fe61be509382dc39d9c8e132
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-20 10:01:25 -0700

    add looping for replacing and appending html elements dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,9 @@ function getFurniture() {
 }
 
 $(document).ready(function () {
-  let searchValue = document.getElementById("searchValue");
+  let searchValue = $('#searchValue').val();
   let promise = getFurniture();
-  promise.then(function(response) {
+  promise.then(function (response) {
     let delivery = "Delivery not available"
     let jsonData = JSON.parse(response);
     for (let i = 0; i < jsonData.body.data.length; i++) {
@@ -33,5 +33,46 @@ $(document).ready(function () {
     }
   }, function (error) {
     console.log(error);
+  });
+
+  $('#searchForm').on('change', function (e) {
+    e.preventDefault();
+    $('#furnitureList').text(
+      $('<p>').html(),
+      $('<h2 class="col-md-2">').html(),
+      $('<div class="col-md-2">').html(),
+      $('<div class="col-md-2">').html(),
+      $('<div class="col-md-2">').html(),
+    );
+    searchValue = $('#searchValue').val();
+    let promise = getFurniture();
+    promise.then(function (response) {
+      let delivery = "Delivery not available"
+      let jsonData = JSON.parse(response);
+      for (let i = 0; i < jsonData.body.data.length; i++) {
+        if (jsonData.body.data[i].deliverable === true) {
+          delivery = "Delivery available";
+        }
+        if (searchValue === jsonData.body.data[i].type) {
+          $('#furnitureList').prepend(
+            $('<p>').html(`<img src=${jsonData.body.data[i].imageUrl}></img>`),
+            $('<h2 class="col-md-2">').html(jsonData.body.data[i].name),
+            $('<div class="col-md-2">').html(jsonData.body.data[i].description),
+            $('<div class="col-md-2">').html(delivery),
+            $('<div class="col-md-2">').html(`Price: $${jsonData.body.data[i].cost}`),
+          );
+        } else if(searchValue === "all") {
+          $('#furnitureList').append(
+            $('<p>').html(`<img src=${jsonData.body.data[i].imageUrl}></img>`),
+            $('<h2 class="col-md-2">').text(jsonData.body.data[i].name),
+            $('<div class="col-md-2">').text(jsonData.body.data[i].description),
+            $('<div class="col-md-2">').text(delivery),
+            $('<div class="col-md-2">').text(`Price: $${jsonData.body.data[i].cost}`),
+          );
+        }
+      }
+    }, function (error) {
+      console.log(error);
+    });
   });
 });
